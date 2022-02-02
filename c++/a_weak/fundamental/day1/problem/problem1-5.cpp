3628ff14120c127dee81849d11ad4a24774cdfd3
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-02-03 02:27:43 +0900

    Add problem1-5.cpp

---
+++ c++/a_weak/fundamental/day1/problem/problem1-5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num1, num2;
+
+    cout << "1つ目の数 : ";
+    cin >> num1;
+    cout << "2つ目の数 : ";
+    cin >> num2;
+
+    cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+    cout << num1 << " - " << num2 << " = " << num1 - num2 << endl;
+
+    return 0;
+}
