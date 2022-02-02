186706168f55064b28dedc0b54ef2cd66d521fea
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-02-03 02:24:50 +0900

    Add problem1-4.cpp

---
+++ c++/a_weak/fundamental/day1/problem/problem1-4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num;
+
+    cout << "数値を入力してください : ";
+    cin >> num;
+    cout << num << "を2倍した数は，" << num * 2 << "です．";
+
+    return 0;
+}
