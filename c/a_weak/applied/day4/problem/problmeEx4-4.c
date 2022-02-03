ec097963183852e02e7616531ec9f33a93b5c675
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-02-03 23:21:31 +0900

    Modify problemEx4-4.c

--- c/a_weak/applied/day4/problem/problmeEx4-4.c
+++ c/a_weak/applied/day4/problem/problmeEx4-4.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <malloc.h>
 
+#define SIZE 6
+
 void main() {
     double *a = NULL;
     int i;
 
-    a = (double*)malloc(sizeof(double) * 3);
-    for (i = 0; i < 3; i++) {
+    a = (double*)malloc(sizeof(double) * SIZE);
+    for (i = 0; i < SIZE; i++) {
         a[i] = 0.1 * i;
     }
 
-    for (i = 0; i < 3; i++) {
+    for (i = 0; i < SIZE; i++) {
         printf("%f ", a[i]);
     }
     printf("\n");
