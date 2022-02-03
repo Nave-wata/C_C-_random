d8422dbdade38e3bb8a5d85d088fcfb5c3bad2db
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-02-03 11:44:10 +0900

    Modify problemEx1-4.c

--- c/a_weak/applied/day1/problem/problemEx1-4.c
+++ c/a_weak/applied/day1/problem/problemEx1-4.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 void main() {
-    int a = 1, b  = 1;
-    printf("%d + %d = %d\n", a, b, a + b);
+    int i, a, b;
+
+    srand((unsigned) time(NULL));
+
+    for (i = 0; i < 6; i++) {
+        a = rand() % 10 + 1;
+        b = rand() % 10 + 1;
+        printf("%d + %d = %d\n", a, b, a + b);
+    }
 }
