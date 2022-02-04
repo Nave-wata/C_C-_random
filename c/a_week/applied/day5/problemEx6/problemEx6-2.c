4ab95d90a986d011d2356f2275413ef36fff0f39
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-02-04 13:49:05 +0900

    Modify problemEx6-2.c

--- c/a_week/applied/day5/problemEx6/problemEx6-2.c
+++ c/a_week/applied/day5/problemEx6/problemEx6-2.c
@@ -3,47 +3,55 @@
 
 #define STUDENT_NUM (5)
 
-char name[STUDENT_NUM][256];    //  学生の名前
-int id[STUDENT_NUM];        //  学生番号
-int sex[STUDENT_NUM];       //  性別(性別 0:男 1:女)
-int grade[STUDENT_NUM];     //  学年
+typedef struct {
+    char name[256];
+    int id;
+    int sex;
+    int grade;
+} student_data;
 
 void main(){
     int i;
+    student_data data[5];
+
     //  名前の設定
-    strcpy(name[0],"青木一");
-    strcpy(name[1],"遠藤京子");
-    strcpy(name[2],"加藤悟");
-    strcpy(name[3],"佐々木八重子");
-    strcpy(name[4],"田中徹");
-    //  学生番号の設定
-    id[0] = 1001;
-    id[1] = 1002;
-    id[2] = 1003;
-    id[3] = 1004;
-    id[4] = 1005;
+    strcpy(data[0].name, "青木一");
+    strcpy(data[1].name, "遠藤京子");
+    strcpy(data[2].name, "加藤悟");
+    strcpy(data[3].name, "佐々木八重子");
+    strcpy(data[4].name, "田中徹");
+
     //  性別の設定
-    sex[0] = 0;
-    sex[1] = 1;
-    sex[2] = 0;
-    sex[3] = 1;
-    sex[4] = 0;
+    data[0].sex = 0;
+    data[1].sex = 1;
+    data[2].sex = 0;
+    data[3].sex = 1;
+    data[4].sex = 0;
+
+    //  学生番号の設定
+    data[0].id = 1001;
+    data[1].id = 1002;
+    data[2].id = 1003;
+    data[3].id = 1004;
+    data[4].id = 1005;
+
     //  学年
-    grade[0] = 1;
-    grade[1] = 1;
-    grade[2] = 1;
-    grade[3] = 1;
-    grade[4] = 1;
+    data[0].grade = 1;
+    data[1].grade = 1;
+    data[2].grade = 1;
+    data[3].grade = 1;
+    data[4].grade = 1;
+
     //  学生のデータの表示
     for(i = 0; i < STUDENT_NUM; i++){
-        printf("名前:%s\n",name[i]);
+        printf("名前:%s\n",data[i].name);
         printf("性別:");
-        if(sex[i] == 0){
+        if(data[i].sex == 0){
             printf("男\n");
         }else{
             printf("女\n");
         }
-        printf("学生番号:%d\n",id[i]);
-        printf("学年:%d\n\n",grade[i]);
+        printf("学生番号:%d\n", data[i].id);
+        printf("学年:%d\n\n", data[i].grade);
     }
 }
