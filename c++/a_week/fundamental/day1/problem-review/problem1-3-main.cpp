1854ef60bb9e7b5e5b7453263a42ea9620ad13ed
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-02-05 19:33:31 +0900

    Modify problem1-3-main.cpp

--- c++/a_week/fundamental/day1/problem-review/problem1-3-main.cpp
+++ c++/a_week/fundamental/day1/problem-review/problem1-3-main.cpp
@@ -3,8 +3,7 @@
 using namespace std;
 
 int main() {
-    cout << "ONE TWO THREE" << endl;
-    cout << "FOUR FIVE SIX" << endl;
+    cout << "ONE TWO THREE" << endl << "FOUR FIVE SIX" << endl;
 
     return 0;
 }
