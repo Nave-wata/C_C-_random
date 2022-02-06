5e04a8a56e869d9e09c120e7e3653272f2bbe253
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-02-06 18:56:25 +0900

    Add list4-1-main.cpp, etc. car.cpp car.h

---
+++ c++/a_week/fundamental/day4/sample/car.h
@@ -0,0 +1,16 @@
+#ifndef _CAR_H_
+#define _CAR_H_
+
+class Car {
+    public:
+        Car();
+        ~Car();
+        void move();
+        void supply(int);
+
+    private:
+        int m_fuel;
+        int m_migration;
+};
+
+#endif // _CAR_H_
