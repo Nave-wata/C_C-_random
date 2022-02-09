72d18e5099f26bc7035ca8917e996777d909f83c
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-02-10 01:43:30 +0900

    Add problem2-1-main.cpp, etc. bar.cpp, foo.cpp, bar.h, foo.h

---
+++ c++/a_week/applied/day2/problem/foo.cpp
@@ -0,0 +1,13 @@
+#include "foo.h"
+#include <iostream>
+
+using namespace std;
+
+void Foo::hoge() {
+    cout << "Foo::hoge" << endl;
+}
+
+void Foo::fuga(Bar* pBar) {
+    Foo* pFoo;
+    Bar* pBar;
+}
---
+++ c++/a_week/applied/day2/problem/problem2-1-main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "bar.h"
+#include "foo.h"
+
+using namespace std;
+
+int main() {
+    Foo* pFoo;
+    Bar* pBar;
+
+    return 0;
+}
