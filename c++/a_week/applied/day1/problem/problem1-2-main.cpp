1a548e12f73fa6c58fbcc37525e21231f014e639
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-02-09 18:53:30 +0900

    Modify problem1-2-main.cpp

--- c++/a_week/applied/day1/problem/problem1-2-main.cpp
+++ c++/a_week/applied/day1/problem/problem1-2-main.cpp
@@ -2,18 +2,18 @@
 
 using namespace std;
 
-void changeNumber(int*);
+void changeNumber(int&);
 
 int main() {
     int n = 5;
 
     cout << "変更前: " << n << endl;
-    changeNumber(&n);
+    changeNumber(n);
     cout << "変更後: " << n << endl;
 
     return 0;
 }
 
-void changeNumber(int* n) {
-    *n *= 2;
+void changeNumber(int& n) {
+    n *= 2;
 }
