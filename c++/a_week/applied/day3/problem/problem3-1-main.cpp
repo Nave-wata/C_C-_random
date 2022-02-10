db2f75acfe5836d92dd8a5d6f52406cfdb287b2b
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-02-10 19:20:55 +0900

    Modify problem3-1-main.cpp

--- c++/a_week/applied/day3/problem/problem3-1-main.cpp
+++ c++/a_week/applied/day3/problem/problem3-1-main.cpp
@@ -3,36 +3,27 @@
 
 using namespace std;
 
-int max(int, int);
-double max(double, double);
-string max(string, string);
-
-int main() {
-    cout << max(1, 2) << endl;
-    cout << max(1.75, 3.12) << endl;
-    string s1 = "aiu", s2 = "eo";
-    cout << max(s1, s2) << endl;
-
-    return 0;
-}
-
-int max(int a, int b) {
+template <typename T>
+T m_max(T a, T b) {
     if (a < b) {
         return b;
     }
     return a;
 }
 
-double max(double a, double b) {
-    if (a < b) {
-        return b;
-    }
-    return a;
-}
-
-string max(string a, string b) {
+template <>
+string m_max(string a, string b) {
     if (a.length() < b.length()) {
         return b;
     }
     return a;
+}
+
+
+int main() {
+    cout << m_max<int>(1, 2) << endl;
+    cout << m_max<double>(1.75, 3.12) << endl;
+    cout << m_max<string>("aiu", "eo") << endl;
+
+    return 0;
 }
