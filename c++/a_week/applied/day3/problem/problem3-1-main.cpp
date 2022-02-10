8ee1fa14851890265164a5f0f90d05e1f7b2a3bf
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-02-10 22:03:14 +0900

    プロトタイプ宣言に変更

--- c++/a_week/applied/day3/problem/problem3-1-main.cpp
+++ c++/a_week/applied/day3/problem/problem3-1-main.cpp
@@ -4,6 +4,19 @@
 using namespace std;
 
 template <typename T>
+T m_max(T a, T b);
+template <>
+string m_max(string a, string b);
+
+int main() {
+    cout << m_max<int>(1, 2) << endl;
+    cout << m_max<double>(1.75, 3.12) << endl;
+    cout << m_max<string>("aiu", "eo") << endl;
+
+    return 0;
+}
+
+template <typename T>
 T m_max(T a, T b) {
     if (a < b) {
         return b;
@@ -17,13 +30,4 @@ string m_max(string a, string b) {
         return b;
     }
     return a;
-}
-
-
-int main() {
-    cout << m_max<int>(1, 2) << endl;
-    cout << m_max<double>(1.75, 3.12) << endl;
-    cout << m_max<string>("aiu", "eo") << endl;
-
-    return 0;
 }
