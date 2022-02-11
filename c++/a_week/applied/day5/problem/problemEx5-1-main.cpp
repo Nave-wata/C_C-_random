11cd72108ca61954a57b3ad2698f941f4d46d261
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-02-11 22:47:49 +0900

    Modify problemEx5-1-main.cpp

--- c++/a_week/applied/day5/problem/problemEx5-1-main.cpp
+++ c++/a_week/applied/day5/problem/problemEx5-1-main.cpp
@@ -8,18 +8,17 @@ using std::endl;
 
 int main() {
     std::map<string, string> map;
-    string english[] = {"cat", "dog", "bird", "tiger"};
-    string japanese[] = {"猫", "犬", "鳥", "トラ"};
+    const string english[] = {"cat", "dog", "bird", "tiger"};
+    const string japanese[] = {"猫", "犬", "鳥", "トラ"};
 
     for (int i = 0; i < 4; i++) {
         map[english[i]] = japanese[i];
     }
 
-    std::map<string, string>::iterator itr;
-    string str;
-
     cout << "英語で動物の名前を入力して下さい: ";
+    string str;
     std::cin >> str;
+
     if (map.find(str) != map.end()) {
         cout << "「" << map[str] << "」です" << endl;
     } else {
