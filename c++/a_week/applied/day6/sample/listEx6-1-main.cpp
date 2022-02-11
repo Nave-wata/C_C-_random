f7f3750facf8cd06afedcbb20bd7c28f77587299
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-02-12 01:17:48 +0900

    Modify listEx6-1-main.cpp

--- c++/a_week/applied/day6/sample/listEx6-1-main.cpp
+++ c++/a_week/applied/day6/sample/listEx6-1-main.cpp
@@ -7,8 +7,10 @@
 int main() {
     Bird *b1 = NULL;
     Bird *b2 = NULL;
+    Bird *b3 = NULL;
     b1 = new Crow();
     b2 = new Chicken();
+    b3 = new Bird();
 
     b1->sing();
     b1->fly();
