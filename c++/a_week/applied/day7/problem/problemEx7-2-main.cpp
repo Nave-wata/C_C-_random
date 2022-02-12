dc12059410ded3c7b867770e55d2fdacea64e9cd
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-02-12 22:10:35 +0900

    完成 problemEx7-2-main.cpp

--- c++/a_week/applied/day7/problem/problemEx7-2-main.cpp
+++ c++/a_week/applied/day7/problem/problemEx7-2-main.cpp
@@ -4,16 +4,16 @@
 
 int main(){
     NewString s1("HelloWorld");
-    cout << s1.getValue() << endl;
+    cout << "s1 = " << s1.getValue() << endl;
 
     NewString s2;
     s2 = s1;    //  値を代入
-    cout << s2.getValue() << endl;
+    cout<< "s2 = " << s2.getValue() << endl;
 
     NewString s3(s2);   //  NewStringを引数としてコンストラクタ呼び出し
-    cout << s3.getValue() << endl;
+    cout << "s3 = " << s3.getValue() << endl;
 
     NewString s4("HELLOWORLD");
-    cout << s4.getValue() << endl;
+    cout<< "s4 = " << s4.getValue() << endl;
     return 0;
 }
