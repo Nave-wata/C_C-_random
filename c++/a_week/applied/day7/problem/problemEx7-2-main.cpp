60f1493ed3035abc79ea013c1a006a8bc72bff0f
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-02-12 22:12:16 +0900

    見た目良く problemEx7-2-main.cpp

--- c++/a_week/applied/day7/problem/problemEx7-2-main.cpp
+++ c++/a_week/applied/day7/problem/problemEx7-2-main.cpp
@@ -5,19 +5,25 @@
 using namespace std;
 
 int main(){
-    NewString s1("HelloWorld"),s2;
+    NewString s1("HelloWorld");
     cout << s1.getValue() << endl;
+
+    NewString s2;
     s2 = s1;    //  値を代入
     cout << s2.getValue() << endl;
+
     NewString s3(s2);   //  NewStringを引数としてコンストラクタ呼び出し
     cout << s3.getValue() << endl;
+
     NewString s4("HELLOWORLD");
     cout << s4.getValue() << endl;
+
     if(s1 == s2){
         cout << "s1==s2" << endl;
     }
     if(s1 == s4){
         cout << "s1==s4" << endl;
     }
+
     return 0;
 }
