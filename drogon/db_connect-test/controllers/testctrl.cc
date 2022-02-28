eae396ad054f24391d08c5341fad263ec44ef74a
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-02-28 17:15:19 +0900

    Add controllers

---
+++ drogon/db_connect-test/controllers/testctrl.cc
@@ -0,0 +1,7 @@
+  GNU nano 4.8                      testctrl.cc
+#include "testctrl.h"
+
+void testctrl::asyncHandleHttpRequest(const HttpRequestPtr& req, std::function<>
+{
+    // write your application logic here
+}
