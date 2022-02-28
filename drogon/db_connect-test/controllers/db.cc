eefd49e8c671b96b41ac99e17cb68a6c3a295f61
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-02-28 17:50:27 +0900

    だめ

--- drogon/db_connect-test/controllers/db.cc
+++ drogon/db_connect-test/controllers/db.cc
@@ -1,11 +1,12 @@
 #include "db.h"
 
-void db::connectDB(const HttpRequestPtr &req, std::function<void(const HttpRequestPtr &)> && callback) const {
-    LOG_DEBUG << "Accessed";
+void db::connectDB(const HttpRequestPtr &req, std::function<void(const HttpRequestPtr &)> && callback, std::string userId) const {
+    LOG_DEBUG << "Accessed: " << userId;
 
     Json::Value ret;
 
     ret["message"] = "Hello";
+    ret["Id"] = userId;
 
     auto resp = HttpResponse::newHttpJsonResponse(ret);
     callback(resp);
