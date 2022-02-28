f9ca5ec73959e51943a5077d7a556604fd278db8
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-02-28 18:21:39 +0900

    接続OK!!

--- drogon/db_connect-test/controllers/db.cc
+++ drogon/db_connect-test/controllers/db.cc
@@ -1,14 +1,12 @@
 #include "db.h"
 
 void db::connectDB(const HttpRequestPtr &req,
-                  std::function<void(const HttpResponsePtr &)> &&callback,
-                  std::string userId) const {
+                  std::function<void(const HttpResponsePtr &)> &&callback) const {
     LOG_DEBUG << "Accessed: ";
 
     Json::Value ret;
 
     ret["message"] = "Hello";
-    ret["Id"] = userId;
 
     auto resp = HttpResponse::newHttpJsonResponse(ret);
     callback(resp);
--- drogon/db_connect-test/controllers/db.h
+++ drogon/db_connect-test/controllers/db.h
@@ -7,17 +7,11 @@ using namespace drogon;
 class db : public drogon::HttpController<db> {
   public:
     METHOD_LIST_BEGIN
-    // use METHOD_ADD to add your custom processing function here;
-    // METHOD_ADD(db::get, "/{2}/{1}", Get); // path is /db/{arg2}/{arg1}
-    // METHOD_ADD(db::your_method_name, "/{1}/{2}/list", Get); // path is /db/{>
-    // ADD_METHOD_TO(db::your_method_name, "/absolute/path/{1}/{2}/list", Get);>
-    METHOD_ADD(db::connectDB, "/{}", Get);
+
+    METHOD_ADD(db::connectDB, "/", Get);
 
     METHOD_LIST_END
-    // your declaration of processing function maybe like this:
-    // void get(const HttpRequestPtr& req, std::function<void (const HttpRespon>
-    // void your_method_name(const HttpRequestPtr& req, std::function<void (con>
+
     void connectDB(const HttpRequestPtr &req,
-                  std::function<void(const HttpResponsePtr &)> &&callback,
-                  std::string userId) const;
+                  std::function<void(const HttpResponsePtr &)> &&callback) const;
 };
