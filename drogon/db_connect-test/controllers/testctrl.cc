a82cb2d7e1122955e338b020db38d77d026346d3
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-02-28 17:20:47 +0900

    これ簡易版やったから削除

--- drogon/db_connect-test/controllers/testctrl.cc
+++
@@ -1,7 +0,0 @@
-  GNU nano 4.8                      testctrl.cc
-#include "testctrl.h"
-
-void testctrl::asyncHandleHttpRequest(const HttpRequestPtr& req, std::function<>
-{
-    // write your application logic here
-}
