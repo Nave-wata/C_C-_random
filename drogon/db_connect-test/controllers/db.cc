e520d5055e31d0781cec9e8a94a1b914e0177a21
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-02-28 19:07:01 +0900

    だめ

--- drogon/db_connect-test/controllers/db.cc
+++ drogon/db_connect-test/controllers/db.cc
@@ -2,14 +2,33 @@
 
 void db::connectDB(const HttpRequestPtr &req,
                   std::function<void(const HttpResponsePtr &)> &&callback) {
-    LOG_DEBUG << "Accessed: ";
+    LOG_DEBUG << "try connect mariadb";
 
-    conn = mysql_init(NULL);
+    try {
+        conn = mysql_init(NULL);
+        if (mysql_real_connect(
+            conn,
+            "192.168.3.100",
+            "test",
+            "0000",
+            "3306",
+            NULL,
+            0
+        )) {
+            LOG_DEBUG << "SUCCESS";
+        } else {
+            LOG_DEBUG << "FAIL"
+        }
+    } catch (char *e) {
+        LOG_DEBUG << "ERROR";
+    }
 
     Json::Value ret;
 
     ret["message"] = "Hello";
 
     auto resp = HttpResponse::newHttpJsonResponse(ret);
+
+    mysql_close(conn);
     callback(resp);
 }
