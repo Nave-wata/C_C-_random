87b0b51642e8241d3cd28d67ead5503a8408ca1b
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-02-28 17:23:56 +0900

    さっきのでも良かったかもしらんけどいろいろ試していこう

---
+++ drogon/db_connect-test/controllers/db.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <drogon/HttpController.h>
+
+using namespace drogon;
+
+class db : public drogon::HttpController<db> {
+  public:
+    METHOD_LIST_BEGIN
+    // use METHOD_ADD to add your custom processing function here;
+    // METHOD_ADD(db::get, "/{2}/{1}", Get); // path is /db/{arg2}/{arg1}
+    // METHOD_ADD(db::your_method_name, "/{1}/{2}/list", Get); // path is /db/{>
+    // ADD_METHOD_TO(db::your_method_name, "/absolute/path/{1}/{2}/list", Get);>
+
+    METHOD_LIST_END
+    // your declaration of processing function maybe like this:
+    // void get(const HttpRequestPtr& req, std::function<void (const HttpRespon>
+    // void your_method_name(const HttpRequestPtr& req, std::function<void (con>
+};
