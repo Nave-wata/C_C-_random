1fa40dc05db0714fa76be277cbb7b2930841d076
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-02-28 14:43:40 +0900

    関数が三塚欄らしい

--- drogon/Mysql.cpp
+++ drogon/Mysql.cpp
@@ -53,35 +53,35 @@ bool Proc::execMain()
         conn = mysql_init(NULL);
 
         // Establish a MySQL connection
-        if (!mysql_real_connect(
+        if (!mariadb_real_connect(
                 conn,
                 MY_HOSTNAME, MY_USERNAME,
                 MY_PASSWORD, MY_DATABASE,
                 MY_PORT_NO, MY_SOCKET, MY_OPT)) {
-            cerr << mysql_error(conn) << endl;
+            cerr << mariadb_error(conn) << endl;
             return false;
         }
 
         // Execute a sql statement
-        if (mysql_query(conn, "SHOW TABLES")) {
-            cerr << mysql_error(conn) << endl;
+        if (mariadb_query(conn, "SHOW TABLES")) {
+            cerr << mariadb_error(conn) << endl;
             return false;
         }
 
         // Get a result set
-        res = mysql_use_result(conn);
+        res = mariadb_use_result(conn);
 
         // Fetch a result set
         cout << "* MySQL - SHOW TABLES in `"
              << MY_DATABASE << "`" << endl;
-        while ((row = mysql_fetch_row(res)) != NULL)
+        while ((row = mariadb_fetch_row(res)) != NULL)
             cout << row[0] << endl;
 
         // Release memories
-        mysql_free_result(res);
+        mariadb_free_result(res);
 
         // Close a MySQL connection
-        mysql_close(conn);
+        mariadb_close(conn);
     } catch (char *e) {
         cerr << "[EXCEPTION] " << e << endl;
         return false;
