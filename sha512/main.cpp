d555b66464f70b30fb712b26efa1f9334f2cf25b
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-03-03 14:47:26 +0900

    なぜこっちでは動く...

--- sha512/main.cpp
+++ sha512/main.cpp
@@ -1,30 +1,13 @@
 #include <iostream>
-#include <time.h>
 #include "SHA512.h"
 
 int main() {
-    int start = clock();
-    std::string text = "Hello world";
+    std::string text = "a";
     SHA512 sha512;
 
-    for (int i = 0; i < 10000; i++) {
-        text = sha512.hash(text);
-    }
+    text = sha512.hash(text);
 
-    int end = clock();
     std::cout << "result: " << text << std::endl;
-    std::cout << "time: "<< double(end - start) / CLOCKS_PER_SEC << std::endl;
 
-        int start = clock();
-    std::string text = "Hello world";
-    SHA512 sha512;
-
-    for (int i = 0; i < 10000; i++) {
-        text = sha512.hash(text);
-    }
-
-    int end = clock();
-    std::cout << "result: " << text << std::endl;
-    std::cout << "time: "<< double(end - start) / CLOCKS_PER_SEC << std::endl;
     return 0;
 }
